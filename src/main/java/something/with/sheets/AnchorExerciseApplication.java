d5674e21394719b936903d6c0a08901420276aff
Lior Mulay <liormulay6@gmail.com>
2025-07-22 13:50:48 +0300

    initial cammit

---
+++ src/main/java/something/with/sheets/AnchorExerciseApplication.java
@@ -0,0 +1,13 @@
+package something.with.sheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnchorExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnchorExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/something/with/sheets/AnchorExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package something.with.sheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchorExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
